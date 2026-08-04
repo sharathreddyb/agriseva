685cc269bad143732a0552a85fadc9c5c3d9ec81
Sharath Reddy Bayaram <sharathreddyb22@gmail.com>
2026-08-04 16:17:23 +0530

    Set up Spring Boot backend and PostgreSQL

---
+++ backend/src/main/java/com/agriseva/AgrisevaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agriseva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrisevaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrisevaBackendApplication.class, args);
+	}
+
+}
