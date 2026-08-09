1ff7b434ead893df77753609ddfa173f05c4ff1b
Sharath Reddy Bayaram <sharathreddyb22@gmail.com>
2026-08-09 22:05:45 +0530

    Add equipment browse and search

---
+++ frontend/src/services/equipmentService.js
@@ -0,0 +1,33 @@
+import { apiRequest } from "./api";
+
+export async function searchEquipment(filters = {}) {
+  const params = new URLSearchParams();
+
+  if (filters.category) {
+    params.append("category", filters.category);
+  }
+
+  if (filters.district) {
+    params.append("district", filters.district);
+  }
+
+  if (filters.village) {
+    params.append("village", filters.village);
+  }
+
+  if (filters.status) {
+    params.append("status", filters.status);
+  }
+
+  if (filters.keyword) {
+    params.append("keyword", filters.keyword);
+  }
+
+  const queryString = params.toString();
+
+  const endpoint = queryString
+    ? `/equipment?${queryString}`
+    : "/equipment";
+
+  return apiRequest(endpoint);
+}
